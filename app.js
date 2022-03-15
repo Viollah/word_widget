8ce27711b8e02661719813710f16e46557ad3349
Viollah <viollah0119@gmai.com>
2022-03-15 22:00:48 +0200

    added highlighter

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
 const sentenceElem = document.querySelector(".sentence");
 const wordCountElem = document.querySelector(".wordCount");
+const submitBtnElem = document.querySelector('.submitBtn');
+const showSentenceElem = document.querySelector('.showSentence');
+
+
 
 sentenceElem.addEventListener("keyup",()=>{
    const wordCount = wordCounter(sentenceElem.value);
    wordCountElem.innerHTML = wordCount;
 })
+
+let wordSelection = wordWidget(); 
+
+const submitSentence = ()=>{
+    
+    const newWords = sentenceElem.value;
+    if(newWords){
+        showSentenceElem.innerHTML = wordSelection.newWord(newWords);
+    }
+
+    let submittedWords = newWords.split(" ");
+
+    let highlighted = '';
+        for (let i = 0; i < submittedWords .length; i++) {
+            const hightlightWord = submittedWords [i];
+            if(hightlightWord.length>4){
+                highlighted += `<mark style= 'color:black'>${hightlightWord}</mark>`
+
+            }else{
+                highlighted += hightlightWord;
+            }
+
+            showSentenceElem.innerHTML = highlighted;
+           
+            
+    }
+
+}
+
+// add event listener on btn to show action onclick
+submitBtnElem.addEventListener('click', submitSentence);
