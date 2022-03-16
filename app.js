23f830fc8c0bde48e296528852cca135db99ed90
Viollah <viollah0119@gmai.com>
2022-03-16 16:08:35 +0200

    added the hightlight and hide functions

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const sentenceElem = document.querySelector(".sentence");
 const wordCountElem = document.querySelector(".wordCount");
 const submitBtnElem = document.querySelector('.submitBtn');
 const showSentenceElem = document.querySelector('.showSentence');
+const hideElem = document.querySelector('#hide');
 
 
 
@@ -16,19 +17,22 @@ const submitSentence = ()=>{
     
     const newWords = sentenceElem.value;
     if(newWords){
+      
         showSentenceElem.innerHTML = wordSelection.newWord(newWords);
     }
-
+  
     let submittedWords = newWords.split(" ");
+    
 
     let highlighted = '';
-        for (let i = 0; i < submittedWords .length; i++) {
-            const hightlightWord = submittedWords [i];
+    
+        for (let i = 0; i < submittedWords.length; i++) {
+            const hightlightWord = submittedWords[i];
             if(hightlightWord.length>4){
-                highlighted += `<mark style= 'color:black'>${hightlightWord}</mark>`
+                highlighted += `<mark style= 'color:black'>${hightlightWord} </mark> `
 
             }else{
-                highlighted += hightlightWord;
+                highlighted +=  hightlightWord + ' ';
             }
 
             showSentenceElem.innerHTML = highlighted;
@@ -40,3 +44,35 @@ const submitSentence = ()=>{
 
 // add event listener on btn to show action onclick
 submitBtnElem.addEventListener('click', submitSentence);
+
+//checkbox 
+
+hide.addEventListener('click',()=>{
+    const newWords = sentenceElem.value;
+    if(newWords){
+      
+        showSentenceElem.innerHTML = wordSelection.newWord(newWords);
+    }
+  
+    let submittedWords = newWords.split(" ");
+    
+
+    let highlighted = '';
+    
+        for (let i = 0; i < submittedWords.length; i++) {
+            const hightlightWord = submittedWords[i];
+            if(hightlightWord.length<5){
+                highlighted += `<mark style= 'color:black'>${hightlightWord} </mark> `
+               
+            }else{
+                highlighted += ' ';
+            }
+
+            showSentenceElem.innerHTML = hightlightWord;
+           
+            
+    }
+   
+})
+
+
