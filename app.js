70090d55c3cf6359e3f6bdcf4c5ddfe7b7a61748
Viollah <viollah0119@gmai.com>
2022-03-21 18:20:41 +0200

    added function for longest word

--- app.js
+++ app.js
@@ -1,81 +1,94 @@
-const sentenceElem = document.querySelector(".sentence");
-const wordCountElem = document.querySelector(".wordCount");
-const submitBtnElem = document.querySelector('.submitBtn');
-const showSentenceElem = document.querySelector('.showSentence');
-const hideElem = document.querySelector('#hide');
-const wordLengthElem = document.querySelector('.wordLength');
-const checkboxElem = document.getElementById('checkbox');
-const shortWordsElem = document.querySelector('.shortWords');
+const getSentence = document.querySelector(".input");
+const submitBtn = document.querySelector(".submit");
+const display = document.querySelector(".sentenceDisplay");
+const longest = document.querySelector(".longestWord")
+const count = document.querySelector(".count");
+const checkBox = document.querySelector(".hide");
+const longerThan5 = document.querySelector(".longerThan5")
+const lessthan5 = document.querySelector(".lessthan5")
+const displaySentence = document.querySelector(".last5sentences");
 
+// get a reference to the template script tag
+var templateSource = document.querySelector(".templateName").innerHTML;
+// compile the template
+var userTemplate = Handlebars.compile(templateSource);
 
-sentenceElem.addEventListener("keyup", () => {
-    const wordCount = wordCounter(sentenceElem.value);
-    wordCountElem.innerHTML = wordCount;
-})
+let wordsArray = []
+let lastEnteredSentenceArray = []
 
-let wordSelection = wordWidget();
+function getWords() {
+    const sentence = getSentence.value
 
-const submitSentence = () => {
+    wordsArray = sentence.trim().split(" ");
+    const highlightedSentence = wordsArray.map(word => {
+        if (word.length > 4) 
+        { return `<mark>${word}</mark>` }
+            return word 
+    })
+    
+    
+    let newSentence = ""
+    for(i=0; i<highlightedSentence.length; i++){
+        newSentence+= highlightedSentence[i] + " " 
+    }
+    
+    //word Count display
+    let wordCount = sentence.split(" ").length
+   
+    count.innerHTML = wordCount
 
-    let highlighted = '';
-
-    const newWords = sentenceElem.value;
-
-    if (newWords) {
-        showSentenceElem.innerHTML = wordSelection.newWord(newWords);
+    //Longest word
+    let sentenceSplit = sentence.split(' ');
+    let longestWord = 0;
+    let longestInString = ""
+    for(var i = 0; i < sentenceSplit.length; i++){
+        if(sentenceSplit[i].length > longestWord){
+	        longestWord = sentenceSplit[i].length;
+            longestInString = sentenceSplit[i];
+        }
+    }
+    // longest.innerHTML = `<markLongest>${longestInString}</markLongest>`;
+    let longestWordArray = sentenceSplit.filter( (word) => {
+        return word.length == longestInString.length
+    })
+    console.log(longestWordArray);
+   
+    longest.innerHTML = `<markLongest>${longestWordArray}</markLongest>`
+    
+    //Checkbox
+    if(checkBox.checked === true){
+        const wordsLongerThan5 = wordsArray.map(word => {
+            if (word.length >= 5) {
+                return `<mark>${word}</mark>` 
+            }
+          })
+        let joinedArray = wordsLongerThan5.join(" ")
+       
+        display.innerHTML = " "
+        lessthan5.innerHTML = joinedArray
+    
+    }else {
+        lessthan5.innerHTML = ""
+        display.innerHTML = newSentence
     }
 
-    let submittedWords = newWords.split(" ");
-
-    for (let i = 0; i < submittedWords.length; i++) {
-        const hightlightWord = submittedWords[i];
-
-        if (hightlightWord.length > 4) {
-            highlighted += `<mark style= 'color:black'>${hightlightWord} </mark> `
+    
+    //Keep track of the last 5 sentences enetred
+    if(lastEnteredSentenceArray.length < 5){
+        if(!lastEnteredSentenceArray.includes(sentence)){
+            lastEnteredSentenceArray.push(sentence)
         }
-
-        else {
-            highlighted += hightlightWord + ' ';
-        }
-
-        showSentenceElem.innerHTML = highlighted;
-
-
+    }else{
+        lastEnteredSentenceArray.shift()
+        
     }
+    console.log(lastEnteredSentenceArray);
+  
+    displaySentence.innerHTML = `${lastEnteredSentenceArray} `
 
+    
 }
-
-// add event listener on btn to show action onclick
-submitBtnElem.addEventListener('click', submitSentence);
-
-//checkbox 
-
-hide.addEventListener('click', () => {
-    const newWords = sentenceElem.value;
-    if (newWords) {
-
-        showSentenceElem.innerHTML = wordSelection.newWord(newWords);
-    }
-
-    let submittedWords = newWords.split(" ");
-
-
-    let highlighted = '';
-
-    for (let i = 0; i < submittedWords.length; i++) {
-        const hightlightWord = submittedWords[i];
-        if (hightlightWord.length >= 5) {
-            highlighted += `<mark style= 'color:black'>${hightlightWord} </mark> `
-
-        } else {
-            highlighted += ' ';
-
-        }
+submitBtn.addEventListener('click', getWords)
 
 
 
-
-    }
-    showSentenceElem.innerHTML = highlighted;
-
-})
--- wordCount.js
+++ wordCount.js
@@ -1,32 +1,3 @@
-function wordCounter(sentence) {
- return sentence.split(" ").length;
+function wordCounter(input) {
+ return input.split(" ").length;
 }
-//function for hightlighting the word with characters greater than 4
-const wordWidget = () =>{
-
-    const newWord = (nextSentence)=>{
- 
-        if(nextSentence != ''){
-            console.log(nextSentence);
-
-            return nextSentence;
-        
-        }
-        
-    }
-
-
-    return{
-        newWord,
-    }
-}
-//function for the checkbox
-function myFunction() {
-    var checkBox = document.getElementById("myCheck");
-    var text = document.getElementById("text");
-    if (checkBox.checked == true){
-      text.style.display = "block";
-    } else {
-       text.style.display = "none";
-    }
-  }
