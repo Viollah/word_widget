c3605cb0cb2fc5dff12343fab60c8c89e3f46ffb
Viollah <viollah0119@gmai.com>
2022-03-26 20:07:29 +0200

    worked on localstorage

--- app.js
+++ app.js
@@ -1,7 +1,53 @@
+
 function WordCounter(sentence){
 
-    const words = sentence.split(" ");
+    const punction= /[.:;,?!"'~[\]{}()<>/@#$%^&*=+_-]/g
+    const words = sentence.replaceAll(punction,'').split(" ");
 
+     //Keep track of the last 5 sentences enetred
+	 if (lastEnteredSentenceArray.length < 5) {
+		if (!lastEnteredSentenceArray.includes(sentence)) {
+			lastEnteredSentenceArray.push(sentence)
+			localStorage.setItem('lastSentences', JSON.stringify(lastEnteredSentenceArray))
+			for (i = 0; i < lastEnteredSentenceArray.length; i++) {
+				var nodeExample = document.createElement("li")
+				var textNode = document.createTextNode(lastEnteredSentenceArray[i])
+				nodeExample.appendChild(textNode)
+			}
+			document.getElementById("previous").appendChild(nodeExample)
+		}
+	  }else {
+		lastEnteredSentenceArray.shift()
+		if (!lastEnteredSentenceArray.includes(sentence)) {
+			lastEnteredSentenceArray.push(sentence)
+			localStorage.setItem('lastSentences', JSON.stringify(lastEnteredSentenceArray))
+			console.log(lastEnteredSentenceArray);
+			for (i = 0; i < lastEnteredSentenceArray.length; i++) {
+				var nodeExample = document.createElement("li")
+				var textNode = document.createTextNode(lastEnteredSentenceArray[i])
+				nodeExample.appendChild(textNode)
+			}
+			document.getElementById("previous").innerHTML = lastEnteredSentenceArray.map(i => `<li>${i}</li>`).join('');
+		}
+	}
+
+	const averageInput = sentence.split(" ").reduce((a, b) => a + b.length, 0)/sentence.split(" ").length
+	const stringArray = lastEnteredSentenceArray.toString()
+	
+	const average = stringArray.split(" ").reduce((a, b) => a + b.length, 0)/stringArray.split(" ").length
+	const round = average.toFixed(2)
+	
+	if (averageInput > round) {
+		console.log("green");
+		dot.classList.remove("orange");
+		dot.classList.add("green");
+	}
+	else if (averageInput < round) {
+		console.log("orange");
+		dot.classList.remove("green");
+		dot.classList.add("orange");
+	}
+   
 	function getWords(){
 
         const wordlist = words.map(word =>{
@@ -21,27 +67,20 @@ function WordCounter(sentence){
              }
          });
          wordlist[longestWord.index].type = "longest";
-    
+
+            
         const longestWords = wordlist.filter(word => word.length === longestWord.length).forEach(word => word.type ="longest");
          return wordlist;
+
+         
     }
 
     function getLongerWords(){
         return getWords().filter(word =>word.length > 4);
+        
     }
 return{
     getWords,
     getLongerWords
 }
 }
-
-//
-// function showTodos(todos){
-// 	tasks.innerHTML = "";
-//     todos.forEach(todo =>{
-// 		const newTask =document.createdElement('li');
-// 		newTask.innerHTML = todo;
-// 		tasks.appendChild(newTask);
-		
-// 	});
-// }
--- words-dom.js
+++ words-dom.js
@@ -4,135 +4,86 @@ const maxWordCountElem = document.querySelector(".maxWordCountElem");
 const errorMessageElem = document.querySelector("errorMessage");
 const wordsElem = document.querySelector(".words");
 const analyzeBtn = document.querySelector(".analyze");
-const tasks = document.querySelector('.tasks');
-const longerThanFour =document.querySelector(".longerThanFour");
-const sentence =document.querySelector(".list");
-const messages = document.querySelector(".sentences");
-const previousSentences = [];
+const longerThanFour = document.querySelector(".longerThanFour");
+const displaySentence = document.querySelector(".last5sentences");
+const displayList = document.getElementById("previous");
+const dot = document.querySelector(".dot")
+let wordsArray=[]
+let lastEnteredSentenceArray = []
 
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
 
+output.innerHTML = slider.value;
 
-
-
-function showWords(words){
-	
-	const wordElements = words.map(word =>{
-		
-	return `<span class="word ${word.type}">${word.word}<span class="small">(${word.length})</span></span>`
-		
-	
-   });
-   wordsElem.innerHTML = wordElements.join("");
-   wordCountElem.innerHTML = words.length;
-
-   
+slider.oninput = function() {
+	output.innerHTML = this.value;
 }
-//
-// let todoList = [];
-// if(localStorage['todos']){
-// 	const todoList = JSON.parse( localStorage['todos']);
-// 	todoList = todoListString.split(",");
-// 	showTodos(todoList);
-// }
 
+if (localStorage['lastSentences']) {
+	lastEnteredSentenceArray = JSON.parse(localStorage.getItem('lastSentences'))
+	console.log("Able to store on Local Storage");
+}
+for (i = 0; i < lastEnteredSentenceArray.length; i++) {
+	var nodeExample = document.createElement("li")
+	var textNode = document.createTextNode(lastEnteredSentenceArray[i])
+	nodeExample.appendChild(textNode)
+}
+document.getElementById("previous").innerHTML = lastEnteredSentenceArray.map(i => `<li>${i}</li>`).join('');
 
-analyzeBtn.addEventListener('click', function(){
+function showWords(words) {
+	const sliderValue = slider.value;
 
-     if(setenceElem.value.trim().length === 0){
-		wordsElem.innerHTML= " ";
-		wordCountElem.innerHTML= 0;
+	const wordElements = words.map(word => {
+		if(sliderValue != 0){
+			if (word.length >= sliderValue) { 
+				return  `<span class="word ${word.type}">${word.word} <span class="small">(${word.length})</span></span>` 
+			}
+
+		}else{
+			return `${word.word}`
+		}
+	});
+	
+	wordsElem.innerHTML = wordElements.join("");
+	wordCountElem.innerHTML = words.length;
+
+	
+
+}
+
+analyzeBtn.addEventListener('click', function () {
+
+	if (setenceElem.value.trim().length === 0) {
+		wordsElem.innerHTML = " ";  
+		wordCountElem.innerHTML = 0;
 		return;
 	}
 
-	const wordCounter =WordCounter(setenceElem.value);
+	const wordCounter = WordCounter(setenceElem.value);
 
-	const words= wordCounter.getWords();
+	const words = wordCounter.getWords();
+
 
 	showWords(words);
-    
-	previousSentences.unshift(words)
-   if(previousSentences.length <= 5){
-          sentence.innerHTML = `Last 5 Sentences: ${previousSentences}, `;
 
-   }
-   messages.innerHTML = `${words},`;
-
-	// if(wordsElem.innerHTML.length === 0){
-	// 	wordCountElem.innerHTML= 0
-	// }else{
-	// 	wordCountElem.innerHTML = words.length;
-	// }
-      
-
-	// todoList.push(task.value);
-
-
-	// localStorage['todos'] = JSON.stringify(todoList);
-
-    //  task.value ="";
-	//  showTodos(todoList); 
-
+	    
 	
+
 });
 
-//
-// function showTodos(todos){
-// 	tasks.innerHTML = "";
-//     todos.forEach(todo =>{
-// 		const newTask =document.createdElement('li');
-// 		newTask.innerHTML = todo;
-// 		tasks.appendChild(newTask);
-		
-// 	});
-// }
+displayList.addEventListener('click',(evnt)=>{
+	//const words = wordCounter.getWords();
+	setenceElem.value = evnt.target.innerHTML;
+	getWords();
+	//showWords(words);
+})
 
-longerThanFour.addEventListener('click',function(event){
-	
-	const wordCounter =WordCounter(setenceElem.value);
+
+longerThanFour.addEventListener('click', function (event) {
+
+	const wordCounter = WordCounter(setenceElem.value);
 	const words = event.target.checked ? wordCounter.getLongerWords() : wordCounter.getWords();
 	showWords(words);
-	
-	// if(event.target.checked){
-	// 	const words=wordCounter.getLongerWords();
-	// 	showWords(words);
-	//  }else{
-	// 	const words = wordCounter.getWords();
-	// 	showWords(words);
-	//  }
-	
-    
-    
+
 });
-//
-const task = document.querySelector('.task');
-const taskBtn = document.querySelector('.addTask');
-// const tasks = document.querySelector('.tasks');
-
-
-// let todoList = [];
-// if(localStorage['todos']){
-// 	const todoList = JSON.parse( localStorage['todos']);
-// 	todoList = todoListString.split(",");
-// 	showTodos(todoList);
-// }
-
-// taskBtn.addEventListener('click', function(){
-// 	todoList.push(task.value);
-
-
-// 	localStorage['todos'] = JSON.stringify(todoList);
-
-//      task.value ="";
-// 	 showTodos(todoList);     
-		   
-// });
-
-// function showTodos(todos){
-// 	tasks.innerHTML = "";
-//     todos.forEach(todo =>{
-// 		const newTask =document.createdElement('li');
-// 		newTask.innerHTML = todo;
-// 		tasks.appendChild(newTask);
-		
-// 	});
-// }
